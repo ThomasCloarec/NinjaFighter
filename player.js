2efd208cf1279daaf6cd0419a61a3e7b89756835
AutoPR <autopr@irgolic.com>
2023-04-12 07:11:05 +0000

    Add boostActive and boostRechargeTime variables

---
+++ player.js
@@ -0,0 +1,9 @@
+class Player {
+ constructor() {
+   this.speed = 1;
+    this.boostActive = false; // To track if the speed boost is currently active
+    this.boostRechargeTime = 0; // To track the remaining time until the speed boost can be used again
+ }
+
+ // Other methods and code...
+}
